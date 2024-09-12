da9d933e12571f8a116a2ce922434cc8051ca2c3
Art Anisimov <mycloppi@gmail.com>
2024-09-12 14:26:52 +0300

    Implemented Vercel-based redirecting API

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const app = require('express')();
-const PORT = 80;
+const express = require("express");
+const app = express();
 const newUrl = 'https://krpano-7b93c.web.app';
 
 app.get('/', (req, res) => {
-    console.log(req.url);
-    res.redirect(newUrl + req.url);
+    console.log(decodeURIComponent(newUrl + req.url));
+    res.redirect(decodeURIComponent(((newUrl + req.url))));
 })
 
-app.listen(PORT);
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
